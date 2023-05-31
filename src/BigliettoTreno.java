e6669ea504bc7a52665e8db97dfdbc0621ff7849
LucaDalRi <euro2xd@gmail.com>
2023-05-31 16:32:15 +0200

    Aggiunti commenti

--- src/BigliettoTreno.java
+++ src/BigliettoTreno.java
@@ -4,32 +4,32 @@ import java.util.Scanner;
 public class BigliettoTreno {
     public static void main(String[] args) {
 //      Istanza scanner inizializzata
-        Scanner ScanAge = new Scanner(System.in);
-        Scanner ScanDistance = new Scanner(System.in);
+        Scanner scanAge = new Scanner(System.in);
+        Scanner scanDistance = new Scanner(System.in);
 //      Input età e salvataggio nella variabile
         System.out.println("Inserisci la tua età");
-        int Age = ScanAge.nextInt();
+        int age = scanAge.nextInt();
 
         System.out.println("Inserisci i chilometri da percorrere");
-        int DistanceKm = ScanDistance.nextInt();
+        int distanceKm = scanDistance.nextInt();
 //      variabili fisse
-        float PricePerKm = 0.21f;
-        float FinalPrice = PricePerKm * DistanceKm;
+        float pricePerKm = 0.21f;
+        float finalPrice = pricePerKm * distanceKm;
 //      Preparo una variabile già con lo sconto
-        float Discount40 = ((FinalPrice * 40) / 100);
-        float Discount20 = ((FinalPrice * 20) / 100);
+        float Discount40 = ((finalPrice * 40) / 100);
+        float Discount20 = ((finalPrice * 20) / 100);
 //      If per incanalare il flusso del codice nel risultato corretto
-        if (Age > 65) {
-            FinalPrice = (FinalPrice - Discount40);
+        if (age > 65) {
+            finalPrice = (finalPrice - Discount40);
 //          Creo un pattern per limitare i decimali del prezzo finale a 2 es. x,xx
-            String formattedPrice = new DecimalFormat("#.00").format(FinalPrice);
+            String formattedPrice = new DecimalFormat("#.00").format(finalPrice);
             System.out.println("Il prezzo totale è di " + formattedPrice + " euro");
-        } else if (Age < 18) {
-            FinalPrice = (FinalPrice - Discount20);
-            String formattedPrice = new DecimalFormat("#.00").format(FinalPrice);
+        } else if (age < 18) {
+            finalPrice = (finalPrice - Discount20);
+            String formattedPrice = new DecimalFormat("#.00").format(finalPrice);
             System.out.println("Il prezzo totale è di " + formattedPrice + " euro");
         } else {
-            String formattedPrice = new DecimalFormat("#.00").format(FinalPrice);
+            String formattedPrice = new DecimalFormat("#.00").format(finalPrice);
             System.out.println("Il prezzo totale è di " + formattedPrice + " euro");
         }
 
