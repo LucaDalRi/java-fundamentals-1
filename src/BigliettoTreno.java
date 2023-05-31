9c77657240b4d39ca39897df990635e247acb907
LucaDalRi <euro2xd@gmail.com>
2023-05-31 16:17:19 +0200

    Aggiunti commenti

--- src/BigliettoTreno.java
+++ src/BigliettoTreno.java
@@ -3,24 +3,25 @@ import java.util.Scanner;
 
 public class BigliettoTreno {
     public static void main(String[] args) {
-
+//      Istanza scanner inizializzata
         Scanner ScanAge = new Scanner(System.in);
         Scanner ScanDistance = new Scanner(System.in);
-
+//      Input età e salvataggio nella variabile
         System.out.println("Inserisci la tua età");
         int Age = ScanAge.nextInt();
 
         System.out.println("Inserisci i chilometri da percorrere");
         int DistanceKm = ScanDistance.nextInt();
-
+//      variabili fisse
         float PricePerKm = 0.21f;
         float FinalPrice = PricePerKm * DistanceKm;
-
+//      Preparo una variabile già con lo sconto
         float Discount40 = ((FinalPrice * 40) / 100);
         float Discount20 = ((FinalPrice * 20) / 100);
-
+//      If per incanalare il flusso del codice nel risultato corretto
         if (Age > 65) {
             FinalPrice = (FinalPrice - Discount40);
+//          Creo un pattern per limitare i decimali del prezzo finale a 2 es. x,xx
             String formattedPrice = new DecimalFormat("#.00").format(FinalPrice);
             System.out.println("Il prezzo totale è di " + formattedPrice + " euro");
         } else if (Age < 18) {
